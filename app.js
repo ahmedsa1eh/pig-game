5ac977d1c9eb1cdc29939f89ee9b13b4bc2806e8
Ahmed Saleh <ahmed1a1a3a100@gmail.com>
2024-02-10 00:57:46 +0200

    add state variable to control the flow

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const currentScore0El = document.querySelector("#current--0");
 const currentScore1El = document.querySelector("#current--1");
 const player0 = document.querySelector(".player--0");
 const player1 = document.querySelector(".player--1");
-
+let Playing = true;
 // hide the dice img when game reload
 diceImg.classList.add("hidden");
 // create the dice randomly
@@ -28,80 +28,86 @@ score0El.textContent = score0;
 score1El.textContent = score1;
 // roll button functionality
 rollBtn.addEventListener("click", function () {
-  // each click reassign the dice
-  dice = Math.trunc(Math.random() * 6) + 1;
-  // display the dice img
-  diceImg.classList.remove("hidden");
-  // get the dice img dynamically based on the current dice number that has been created
-  diceImg.src = `./images/dice-${dice}.png`;
-  // check the dice number
-  // if it is not = 1
-  if (dice != 1) {
-    // we check the active player and add the dice to the current score and  display its current score
-    switch (player0.classList.contains("player--active")) {
-      case true:
-        currentScore0 += dice;
-        currentScore0El.textContent = `${currentScore0}`;
-        break;
-      case false:
-        currentScore1 += dice;
-        currentScore1El.textContent = `${currentScore1}`;
+  if (Playing) {
+    // each click reassign the dice
+    dice = Math.trunc(Math.random() * 6) + 1;
+    // display the dice img
+    diceImg.classList.remove("hidden");
+    // get the dice img dynamically based on the current dice number that has been created
+    diceImg.src = `./images/dice-${dice}.png`;
+    // check the dice number
+    // if it is not = 1
+    if (dice != 1) {
+      // we check the active player and add the dice to the current score and  display its current score
+      switch (player0.classList.contains("player--active")) {
+        case true:
+          currentScore0 += dice;
+          currentScore0El.textContent = `${currentScore0}`;
+          break;
+        case false:
+          currentScore1 += dice;
+          currentScore1El.textContent = `${currentScore1}`;
+      }
+      // if (player0.classList.contains = "player--active") {
+      //   currentScore0 += dice;
+      //   currentScore0El.textContent = `${currentScore0}`;
+      // } else {
+      //   currentScore1 += dice;
+      //   currentScore1El.textContent = `${currentScore1}`;
+      // }
     }
-    // if (player0.classList.contains = "player--active") {
-    //   currentScore0 += dice;
-    //   currentScore0El.textContent = `${currentScore0}`;
-    // } else {
-    //   currentScore1 += dice;
-    //   currentScore1El.textContent = `${currentScore1}`;
-    // }
-  }
-  // if dice = 1
-  else {
-    // check the active player and reset his current score to 0 and display it then switch player
-    if (player0.classList.contains("player--active")) {
-      currentScore0 = 0;
-      currentScore0El.textContent = currentScore0;
-      player0.classList.remove("player--active");
-      player1.classList.add("player--active");
-    } else {
-      currentScore1 = 0;
-      currentScore1El.textContent = currentScore0;
-      player1.classList.remove("player--active");
-      player0.classList.add("player--active");
+    // if dice = 1
+    else {
+      // check the active player and reset his current score to 0 and display it then switch player
+      if (player0.classList.contains("player--active")) {
+        currentScore0 = 0;
+        currentScore0El.textContent = currentScore0;
+        player0.classList.remove("player--active");
+        player1.classList.add("player--active");
+      } else {
+        currentScore1 = 0;
+        currentScore1El.textContent = currentScore0;
+        player1.classList.remove("player--active");
+        player0.classList.add("player--active");
+      }
     }
   }
 });
 
 // hold button
 holdBtn.addEventListener("click", function () {
-  // check the active player and if its player one is active
-  if (player0.classList.contains("player--active")) {
-    //  add the current score to his score and switch to player two
-    score0 += currentScore0;
-    score0El.textContent = score0;
-    currentScore0 = 0;
-    currentScore0El.textContent = currentScore0;
-    player0.classList.remove("player--active");
-    player1.classList.add("player--active");
-    // we check player one score and if its >= 100 we give him the winner class
-    if (score0 >= 100) {
-      player0.classList.add("player--winner");
+  if (Playing) {
+    // check the active player and if its player one is active
+    if (player0.classList.contains("player--active")) {
+      //  add the current score to his score and switch to player two
+      score0 += currentScore0;
+      score0El.textContent = score0;
+      currentScore0 = 0;
+      currentScore0El.textContent = currentScore0;
       player0.classList.remove("player--active");
+      player1.classList.add("player--active");
+      // we check player one score and if its >= 100 we give him the winner class
+      if (score0 >= 10) {
+        player0.classList.add("player--winner");
+        player0.classList.remove("player--active");
+        Playing = false;
+      }
     }
-  }
 
-  // if the player 2 is the active player
-  else {
-    // add the current score to his score and switch to player one
-    score1 += currentScore1;
-    score1El.textContent = score1;
-    currentScore1 = 0;
-    currentScore1El.textContent = currentScore1;
-    player0.classList.add("player--active");
-    player1.classList.remove("player--active");
-    // we check player two score and if its >= 100 we give him the winner class
-    if (score1 >= 100) {
-      player1.classList.add("player--winner");
+    // if the player 2 is the active player
+    else {
+      // add the current score to his score and switch to player one
+      score1 += currentScore1;
+      score1El.textContent = score1;
+      currentScore1 = 0;
+      currentScore1El.textContent = currentScore1;
+      player0.classList.add("player--active");
+      player1.classList.remove("player--active");
+      // we check player two score and if its >= 100 we give him the winner class
+      if (score1 >= 10) {
+        player1.classList.add("player--winner");
+        Playing = false;
+      }
     }
   }
 });
@@ -123,4 +129,5 @@ newGameBtn.addEventListener("click", function () {
   diceImg.classList.add("hidden");
   player0.classList.remove("player--winner");
   player1.classList.remove("player--winner");
+  Playing = true;
 });
